1202f1ea132bdf309557c40f85be7060bfa5bf3a
92scoupe <131972382+92scoupe@users.noreply.github.com>
2023-05-01 14:50:10 +0900

    feat : add if sentence to set range 1 to 100

--- lucky.py
+++ lucky.py
@@ -1,16 +1,24 @@
 # Select  Six Lucky Number!
 import random
 
+# insert loop number
 input_num = int(input("Insert Loop number : "))
+ 
+# Error Correction
+if input_num > 100:
+    print("input below integer 100")
+       
+elif input_num < 1:
+    print("input over integer zero")
 
-# Set number
-set_numbers = 6
+else: 
+    set_numbers = 6
 
-# Choose six numbers in 45.
-lucky_list =[]
+# make a list of lucky numbers
+    lucky_list =[]
 
-for i in range(input_num):
-    lucky = random.sample(range(1, 46), set_numbers)
-    lucky_list.append(lucky)
-    print(lucky_list[i])
+    for i in range(input_num):
+        lucky = random.sample(range(1, 46), set_numbers)
+        lucky_list.append(lucky)
+        print(lucky_list[i])
 
